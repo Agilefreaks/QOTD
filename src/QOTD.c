6b53ae66068e51eb053cff64bd58d25c4ac480b9
Denisa1997 <denisa_maria1997@yahoo.com>
2015-08-06 15:06:10 +0300

    add quote layer   add author layer

--- src/QOTD.c
+++ src/QOTD.c
@@ -1,20 +1,21 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *quote_layer;
+static TextLayer *author_layer;
 static GBitmap *quote_bitmap;
 static BitmapLayer *image_layer;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+  text_layer_set_text(quote_layer, "Select");
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+  text_layer_set_text(quote_layer, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+  text_layer_set_text(quote_layer, "Down");
 }
 
 static void click_config_provider(void *context) {
@@ -31,23 +32,33 @@ static void add_bitmap_layer(Layer *window_layer){
   layer_add_child(window_layer, quote_layer);
 }
 
-  //add_text_layer()
-static void add_text_layer (Layer *window_layer){
-  GRect bounds = layer_get_bounds(window_layer);
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-}
-static void window_load(Window *window) {
+static void add_text_layer_for_window(TextLayer *text_layer, char *text, GRect rect, GTextAlignment alignment) {
   Layer *window_layer = window_get_root_layer(window);
 
+  text_layer = text_layer_create(rect);
+  text_layer_set_text(text_layer, text);
+  text_layer_set_text_alignment(text_layer, alignment);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void add_quote_layer(Layer *window_layer){
+  add_text_layer_for_window(quote_layer,"Listening is a magnetic and strange thing, a creative force. The friends who listen to us are the ones we move toward. When we are listened to, it creates us, makes us unfold and expand.",GRect(0, 50, 144, 88) ,GTextAlignmentCenter);
+}
+
+static void add_author_layer(Layer *window_layer){
+  add_text_layer_for_window(author_layer, "--Karl A. Menniger", GRect(0, 140, 144, 90), GTextAlignmentCenter);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
   add_bitmap_layer(window_layer);
-  add_text_layer(window_layer);
+  add_quote_layer(window_layer);
+  add_author_layer(window_layer);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(quote_layer);
+  text_layer_destroy(author_layer);
 }
 
 static void init(void) {
