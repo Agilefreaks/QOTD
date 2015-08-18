e6f1e7cb40b4d00e903d3866cf30856f131a6e99
freebeerwtf <mihu_teodora@yahoo.com>
2015-08-18 15:58:56 +0300

    [QOTD-17] Add quote screen in a scroll layer

--- src/QOTD.c
+++ src/QOTD.c
@@ -1,63 +1,67 @@
 #include <pebble.h>
 
 static Window *window;
+static ScrollLayer *scroll_layer;
 static TextLayer *quote_layer;
 static TextLayer *author_layer;
 static GBitmap *quote_bitmap;
 static BitmapLayer *image_layer;
 static GFont author_font;
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(quote_layer, "Select");
-}
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(quote_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(quote_layer, "Down");
-}
+static char scroll_text[] = "Listening is a magnetic and strange thing, a creative force. The friends who listen to us are the ones we move toward. When we are listened to, it creates us, makes us unfold and expand.Listening is a magnetic and strange thing, a creative force. The friends who listen to us are the ones we move toward. When we are listened to, it creates us, makes us unfold and expand.";
 
 static void click_config_provider(void *context) {
 }
 
-static void add_bitmap_layer(Layer *window_layer){
+static void add_bitmap_layer(ScrollLayer *scroll_layer){
   quote_bitmap = gbitmap_create_with_resource(RESOURCE_ID_QUOTE);
   image_layer = bitmap_layer_create(GRect(0, 0, 144, 40));
   bitmap_layer_set_bitmap(image_layer,quote_bitmap );
   Layer *quote_layer = bitmap_layer_get_layer(image_layer);
-  layer_add_child(window_layer, quote_layer);
+  scroll_layer_add_child(scroll_layer, quote_layer);
 }
 
 static void prepare_text_layer(TextLayer *text_layer, char *text, GRect rect, GTextAlignment alignment) {
 
 }
 
-static void add_quote_layer(Layer *window_layer){
-  quote_layer = text_layer_create(GRect(0, 41, 144, 88));
-  text_layer_set_text(quote_layer, "Listening is a magnetic and strange thing, a creative force. The friends who listen to us are the ones we move toward. When we are listened to, it creates us, makes us unfold and expand.");
+static void add_quote_layer(ScrollLayer *scroll_layer){
+  GRect max_text_bounds = GRect(0, 41, 144, 2000);
+  quote_layer = text_layer_create(max_text_bounds);
+  text_layer_set_text(quote_layer, scroll_text);
   text_layer_set_text_alignment(quote_layer, GTextAlignmentCenter);
 
-  layer_add_child(window_layer, text_layer_get_layer(quote_layer));
+  GSize max_size = text_layer_get_content_size(quote_layer);
+  text_layer_set_size(quote_layer, max_size);
+  scroll_layer_set_content_size(scroll_layer, GSize(144, max_size.h + 4));
+
+  scroll_layer_add_child(scroll_layer, text_layer_get_layer(quote_layer));
 }
 
-static void add_author_layer(Layer *window_layer){
-  author_layer = text_layer_create(GRect(0, 130, 144, 30));
+static void add_author_layer(ScrollLayer *scroll_layer){
+  GRect max_text_bounds = GRect(0, 0, 144, 2000);
+  author_layer = text_layer_create(max_text_bounds);
   text_layer_set_text(author_layer, "Karl A. Menniger");
   text_layer_set_text_alignment(author_layer, GTextAlignmentCenter);
 
   author_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_AUTHOR_25));
   text_layer_set_font(author_layer, author_font);
 
-  layer_add_child(window_layer, text_layer_get_layer(author_layer));
+  scroll_layer_add_child(scroll_layer, text_layer_get_layer(author_layer));
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  add_bitmap_layer(window_layer);
-  add_quote_layer(window_layer);
-  add_author_layer(window_layer);
+  GRect bounds = layer_get_frame(window_layer);
+  GRect max_text_bounds = GRect(0, 0, 144, 2000);
+
+  scroll_layer = scroll_layer_create(bounds);
+  scroll_layer_set_click_config_onto_window(scroll_layer, window);
+
+  add_bitmap_layer(scroll_layer);
+  add_quote_layer(scroll_layer);
+  //add_author_layer(scroll_layer);
+
+  layer_add_child(window_layer, scroll_layer_get_layer(scroll_layer));
 }
 
 
